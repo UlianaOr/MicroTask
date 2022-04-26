436b19bb4a7a4b243bff637edd7c3ca58d5c36f7
UlianaOr <Ouliana9@gmail.com>
2022-04-26 13:35:42 +0200

    as

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Body } from './site/Body';
 import {Header}  from './site/Header';
 
 function App() {
     return (
-        
+        <>
         <Header/>
+        <Body/>
+        </>
     );
 }
 
