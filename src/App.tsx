95a3b3cec13511ea65180e340227de1ea7019f93
UlianaOr <Ouliana9@gmail.com>
2022-04-27 13:11:54 +0200

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Body } from './site/Body';
 import {Header}  from './site/Header';
+import {Footer}  from './site/Footer';
 
 function App() {
     return (
         <>
         <Header title = {"New Body"}/>
         <Body titleFofBody ={"New Body For!"}/>
+        <Footer titleForFooter ={"New Footer For!"}/>
         </>
     );
 }
--- src/site/Body.tsx
+++ src/site/Body.tsx
@@ -8,4 +8,4 @@ export const Body=(props:BodyType)=> {
     return (
         <>{props.titleFofBody}</>
     )
-}
+} 
