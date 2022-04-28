1ed4a7e094a91428568719d76e7d8f1ed8c4caa7
UlianaOr <Ouliana9@gmail.com>
2022-04-28 10:21:49 +0200

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import {Header}  from './site/Header';
 import {Footer}  from './site/Footer';
 
 function App() {
+    const myFirstSubscriber = () => {
+        console.log ("Hello Im Vasia")
+
+    }
     return (
         <>
         <Header title = {"New Body"}/>
         <Body titleFofBody ={"New Body For!"}/>
         <Footer titleForFooter ={"New Footer For!"}/>
 
-        <button onClick={()=>{console.log("Hello")}}>MyYouTubeChanel-1</button>
-        <button onClick={()=>{console.log("Hello")}}>MyYouTubeChanel-2</button>
+         {/*<button onClick={()=>{console.log("Hello")}}>MyYouTubeChanel-1</button>*/}
+        <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
 
         </>
     );
