b6dc595936aec3714ce97879855446dc1d3d9f94
UlianaOr <Ouliana9@gmail.com>
2022-05-02 16:06:31 +0200

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Body } from './site/Body';
-import {Header}  from './site/Header';
-import {Footer}  from './site/Footer';
+
 
 function App() {
    
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Button =() =>{
+    return(
+        <button> MyYouTubeChanel -1</button>
+
+
+    )
+}
