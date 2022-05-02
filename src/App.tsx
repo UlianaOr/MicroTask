7d96de0e02d99df55096aa32a095968898a9f303
UlianaOr <Ouliana9@gmail.com>
2022-05-02 15:59:01 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,11 @@ import {Footer}  from './site/Footer';
 function App() {
    
 
-       const foo1 =()=>{
-           console.log (100200)
-       }
-
-       const foo2 =(num:number) =>{
-           console.log(num)
-       }
-
+       
     return (
        <div className='App'>
-           <button onClick={foo1}>1</button>
-           <button onClick={()=>foo2(100200)}>2</button>
+           <button>MyYouTubeChanel -1</button>
+           <button>MyYouTubeChanel -2</button>
            
             
         </div>
