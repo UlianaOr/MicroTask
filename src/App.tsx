d7e5a0cfc7140e92aa7d9a0ab3c7bc0c1de9395b
UlianaOr <Ouliana9@gmail.com>
2022-05-03 09:59:01 +0200

    qw

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import { Button } from './components/Button';
 
 
 function App() {
+    const Button1Foo =()=> {
+        console.log ("I am Uliana")
+
+    }
+    
+    const Button2Foo =()=> {
+        console.log ("I am Jony")
+
+    }
    
 
        
     return (
        <div className='App'>
           
-            <Button name={'MyYouTubeChanel-1'}/>
-           <Button name={'MyYouTubeChanel-2'}/>
+            <Button name={'MyYouTubeChanel-1'} callBack ={Button1Foo}/>
+           <Button name={'MyYouTubeChanel-2'} callBack ={Button2Foo}/>
             
         </div>
     );
